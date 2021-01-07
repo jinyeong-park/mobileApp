86a4f0f774c73870fdefce91651b9eaab4886191
Jinyeong Park <jinyeongpark1205@gmail.com>
2021-01-07 10:50:45 -0800

    Change inline styles to stylesheet object

--- App.js
+++ App.js
@@ -4,18 +4,31 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{padding: 50}}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-        <TextInput placeholder="Course Goal" style={{borderColor: 'black', borderWidth: 1}}/>
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.textInput}/>
         <Button title="ADD"/>
         <Text></Text>
       </View>
       <View>
         {/* list of goals */}
       </View>
-
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  textInput: {
+    borderColor: 'black', borderWidth: 1
+  }
+});
 
 
