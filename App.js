37d35ae857a10d2bca067528d0970cbf859de4ea
Jinyeong Park <jinyeongpark1205@gmail.com>
2021-01-07 18:06:54 -0800

    Write goals and show goals - events for onChangeText & onPress

--- App.js
+++ App.js
@@ -1,17 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  }
+
+  const addGoalHandler = () => {
+    console.log(enteredGoal)
+    // courseGoals doesn't always garantee
+    //setCourseGoal([...courseGoals, enteredGoal])
+    setCourseGoals(currentGoals => [...courseGoals, enteredGoal]);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Course Goal" style={styles.textInput}/>
-        <Button title="ADD"/>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler}/>
         <Text></Text>
       </View>
       <View>
         {/* list of goals */}
+        {courseGoals.map(goal => <Text>{goal}</Text>)}
       </View>
     </View>
   );
